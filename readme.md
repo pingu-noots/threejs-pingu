0e1d22e959da0bcf067b228d8c31cc6b69cdcf49
pingue-noots <high.bridge031900@gmail.com>
2022-09-14 00:14:58 +0900

    firstCommit

--- readme.md
+++ readme.md
@@ -1,17 +1,21 @@
-# Three.js Starter
-Courtesy of Bruno Simon of https://threejs-journey.xyz/
-
 ## Setup
-Download [Node.js](https://nodejs.org/en/download/).
-Run this followed commands:
 
-``` bash
-# Install dependencies (only the first time)
+Download [Node.js](https://nodejs.org/en/download/).
+Run this commands:
+
+```bash
 npm install
 
-# Run the local server at localhost:8080
+# port is 8080
 npm run dev
 
 # Build for production in the dist/ directory
 npm run build
+
+# Not work? Check updates
+npm install -g npm-check-updates
+
+ncu -u
+
+npm update
 ```
--- src/script.js
+++ src/script.js
@@ -1,105 +1,219 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
+import "./style.css";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "dat.gui";
+
+// Loading
+const textureLoader = new THREE.TextureLoader();
+
+const normalTexture = textureLoader.load("/textures/4k_map.png");
 
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI();
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
+
+// animation
+let mixer;
+let clock = new THREE.Clock();
+const actions = [];
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+// const sphereGeometry = new THREE.SphereGeometry(0.5, 64, 64);
+
+// pingu load
+const loader = new GLTFLoader();
+const url = "/textures/pingu.glb";
+let model = null;
+
+loader.load(
+  url,
+  function (gltf) {
+    model = gltf.scene;
+    model.position.set(0, -0.5, 0);
+    scene.add(model);
+
+    //ANIMATION
+    mixer = new THREE.AnimationMixer(model);
+    // 複数のアクションすべてを再生
+    gltf.animations.forEach((animation) => {
+      actions.push(mixer.clipAction(animation).play());
+    });
+
+    tick();
+  },
+  function (error) {
+    console.log("An error happened");
+    console.log(error);
+  }
+);
 
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+// const material = new THREE.MeshStandardMaterial();
+// material.metalness = 0.7;
+// material.roughness = 0.2;
+// material.normalMap = normalTexture;
+
+// material.color = new THREE.Color(0x292929);
 
 // Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+// const sphere = new THREE.Mesh(sphereGeometry, material);
+// scene.add(sphere);
 
-// Lights
+// Lights 1
+const pointLight = new THREE.PointLight(0xffffff, 0.1);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+// Lights 2
+const pointLight2 = new THREE.PointLight(0xffffff, 2);
+pointLight2.position.set(-1.86, 1, -1.65);
+pointLight2.intensity = 7;
+
+scene.add(pointLight2);
+
+// const light1 = gui.addFolder("Light 1");
+
+// light1.add(pointLight2.position, "y").min(-3).max(3).step(0.01);
+// light1.add(pointLight2.position, "x").min(-3).max(3).step(0.01);
+// light1.add(pointLight2.position, "z").min(-3).max(3).step(0.01);
+// light1.add(pointLight2, "intensity").min(0).max(10).step(0.01);
+
+// const pointLightHelper = new THREE.PointLightHelper(pointLight2, 1);
+// scene.add(pointLightHelper);
+
+// Lights 3
+const pointLight3 = new THREE.PointLight(0xe1ff, 2);
+pointLight3.position.set(2.13, -3, -1.98);
+pointLight3.intensity = 6.8;
+
+scene.add(pointLight3);
+// const light2 = gui.addFolder("Light 2");
+
+// light2.add(pointLight3.position, "y").min(-3).max(3).step(0.01);
+// light2.add(pointLight3.position, "x").min(-3).max(3).step(0.01);
+// light2.add(pointLight3.position, "z").min(-3).max(3).step(0.01);
+// light2.add(pointLight3, "intensity").min(0).max(10).step(0.01);
+
+// const light2Color = {
+//   color: 0xff0000,
+// };
+
+// light2.addColor(light2Color, "color").onChange(() => {
+//   pointLight3.color.set(light2Color.color);
+// });
+
+// const pointLightHelper2 = new THREE.PointLightHelper(pointLight3, 1);
+// scene.add(pointLightHelper2);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 2;
+scene.add(camera);
 
 // Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+  alpha: true,
+  antialias: true,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
 
-const clock = new THREE.Clock()
+let mouseX = 0;
+let mouseY = 0;
 
-const tick = () =>
-{
+let targetX = 0;
+let targetY = 0;
 
-    const elapsedTime = clock.getElapsedTime()
+const windowHalfX = window.innerWidth / 2;
+const windowHalfY = window.innerHeight / 2;
 
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+const onDocumentMouseMove = (event) => {
+  mouseX = event.clientX - windowHalfX;
+  mouseY = event.clientY - windowHalfY;
+};
 
-    // Update Orbital Controls
-    // controls.update()
+const updateSphere = (event) => {
+  model.position.y = window.scrollY * 0.001;
+};
+// window.addEventListener("scroll", updateSphere);
 
-    // Render
-    renderer.render(scene, camera)
+document.addEventListener("mousemove", onDocumentMouseMove);
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+const tick = () => {
+  targetX = mouseX * 0.001;
+  targetY = mouseY * 0.001;
 
-tick()
+  const delta = clock.getDelta();
+  mixer.update(delta);
+  // const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+  // model.rotation.y = 0.5 * elapsedTime;
+
+  // sphere.rotation.y += 0.5 * (targetX - sphere.rotation.y);
+  // sphere.rotation.x += 0.05 * (targetY - sphere.rotation.x);
+  // sphere.position.z += -0.05 * (targetY - sphere.rotation.x);
+
+  // Update Orbital Controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+// tick();
